d1a0dfc165dd57568555559d6379513c83f80533
charliewhu <33449114+charliewhu@users.noreply.github.com>
2020-09-08 14:51:50 +0100

    create folder for unit 3

---
+++ Unit 2 Exercises - Linear Regression/State/State_Data.R
@@ -0,0 +1,13 @@
+
+
+data(state) #get inbuilt state dataset 
+
+statedata = cbind(data.frame(state.x77), state.abb, state.area, state.center,  state.division, state.name, state.region)
+#convert to dataframe
+
+
+
+
+
+
+
